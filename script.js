2dfb4a2367350c5153880fe805c4d27197e5f371
Lucifer-mk <manojkaprimanoj@gmail.com>
2024-12-20 16:39:34 +1100

    some function added

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-document.getElementById("display").innerText = "Hello World!";
+document.getElementById("display").value = "";
+
+
+
+document.getElementById("clearscreen").onclick = function() {
+  document.getElementById("display").value = "";
+}
+document.getElementById("openbraces1").onclick = function() {
+    document.getElementById("display").value += "(";
+}
+document.getElementById("openbraces2").onclick = function() {
+    document.getElementById("display").value += ")";
+}
+document.getElementById("percentage").onclick = function() {
+    document.getElementById("display").value += "%";
+}
+document.getElementById("factorial").onclick = function() {
+    document.getElementById("display").value += "!";
+}
+document.getElementById("sin").onclick = function() {
+    document.getElementById("display").value += "sin(";
+}
+document.getElementById("devide").onclick = function() {
+    document.getElementById("display").value += "/";
+}
+document.getElementById("seven").onclick = function() {
+    document.getElementById("display").value += "7";
+}
+document.getElementById("eight").onclick = function() {
+    document.getElementById("display").value += "8";
+}
+document.getElementById("nine").onclick = function() {
+    document.getElementById("display").value += "9";
+}
+document.getElementById("log").onclick = function() {
+    document.getElementById("display").value += "log(";
+}
+document.getElementById("cos").onclick = function() {  
+    document.getElementById("display").value += "cos(";
+}
+document.getElementById("multiply").onclick = function() {  
+    document.getElementById("display").value += "*";
+}
+document.getElementById("four").onclick = function() {
+    document.getElementById("display").value += "4";
+}
+document.getElementById("five").onclick = function() {
+    document.getElementById("display").value += "5";
+}
+document.getElementById("six").onclick = function() {
+    document.getElementById("display").value += "6";
+}   
+document.getElementById("pie").onclick = function() {
+    document.getElementById("display").value += "π";
+
+}
+document.getElementById("tan").onclick = function() {
+    document.getElementById("display").value += "tan(";
+}
+document.getElementById("minus").onclick = function() {
+    document.getElementById("display").value += "-";
+}
+document.getElementById("one").onclick = function() {
+    document.getElementById("display").value += "1";
+}
+document.getElementById("two").onclick = function() {
+    document.getElementById("display").value += "2";
+}
+document.getElementById("three").onclick = function() {
+    document.getElementById("display").value += "3";
+}
+document.getElementById("square").onclick = function() {
+    document.getElementById("display").value += "x^2";
+}
+document.getElementById("squrt").onclick = function() {
+    document.getElementById("display").value += "√";
+}
